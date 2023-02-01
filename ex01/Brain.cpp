7124587c1369033a0b71b0b7f9b48c225c230968
sdxsd <willmaguire@icloud.com>
2023-02-01 19:50:06 +0100

    Brein.cpp

---
+++ ex01/Brain.cpp
@@ -0,0 +1,24 @@
+#include "Brain.hpp"
+
+Brain::Brain(void) {
+	std::cout << "Brain constructor called." << std::endl;
+	for (int i = 0; i < 100; i++)
+		ideas[i].clear();
+}
+
+Brain::Brain(const Brain& squishy) {
+	std::cout << "Brain copy constructor called." << std::endl;
+	for (int i = 0; i < 100; i++)
+		ideas[i] = squishy.ideas[i];
+}
+
+Brain::~Brain(void) {
+	std::cout << "Brain destructor called." << std::endl;
+}
+
+Brain& Brain::operator=(const Brain& toCopy) {
+	std::cout << "Brain = operator called." << std::endl;
+	for (int i = 0; i < 100; i++)
+		this->ideas[i] = toCopy.ideas[i];
+	return (*this);
+}
