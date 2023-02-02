0b992a9a38cb26ed490761d0fb4ea5b4368dc447
sdxsd <willmaguire@icloud.com>
2023-02-02 15:34:50 +0100

    Lots of animals.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,7 +1,7 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-void copy_test(void) {
+void brain_test(void) {
 	Animal* Fred = new Cat();
 	Animal* Saartje = new Cat();
 
@@ -17,16 +17,16 @@ void copy_test(void) {
 }
 
 int main(void) {
-	// const Animal* meta = new Animal();
-	// const Animal* j = new Dog();
-	// const Animal* i = new Cat();
-	// std::cout << j->getType() << " " << std::endl;
-	// std::cout << i->getType() << " " << std::endl;
-	// i->makeSound();
-	// j->makeSound();
-	// meta->makeSound();
-	// delete meta;
-	// delete j;
-	// delete i;
-	copy_test();
+	// brain_test();
+	Animal *zoo[100];
+	for (int i = 0; i < 49; i++)
+		zoo[i] = new Dog();
+	for (int i = 49; i < 100; i++)
+		zoo[i] = new Cat();
+	zoo[49]->makeSound();
+	zoo[99]->makeSound();
+	for (int i = 0; i < 49; i++)
+		delete (Dog*)zoo[i];
+	for (int i = 49; i < 100; i++)
+		delete (Animal*)zoo[i];
 }
