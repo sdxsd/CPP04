012bfc0b1183039bf61586ee16c16329805a44af
sdxsd <willmaguire@icloud.com>
2023-03-03 20:32:04 +0100

    Goed.

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,16 +2,28 @@
 #include "Cat.hpp"
 
 void brain_test(void) {
-	Cat* Fred = new Cat();
-	Cat* Saartje = new Cat();
+	int		fail = false;
+	Animal* Fred = new Cat();
+	Animal* Saartje = new Cat();
 
 	Fred->setIdeaInBrain(0, "I want to eat Saartjes food.");
 	std::cout << "Fred has idea: " << Fred->getIdeaFromBrain(0) << " at position 0" << std::endl;
 	Saartje->setIdeaInBrain(0, "I don't want Fred to eat my food.");
 	std::cout << "Saartje has idea: " << Saartje->getIdeaFromBrain(0) << " at position 0" << std::endl;
 	*Fred = *Saartje;
-	std::cout << "\"after *Fred = *Saartje;\" Fred has idea: " << Fred->getIdeaFromBrain(0) << " at position 0"
+	std::cout << "after \"*Fred = *Saartje;\" Fred has idea: " << Fred->getIdeaFromBrain(0) << " at position 0"
 			  << std::endl;
+
+	for (int i = 0; i < 100; i++)
+		Fred->setIdeaInBrain(i, "FOOD!");
+	*Saartje = *Fred;
+	for (int i = 0; i < 100; i++)
+		if (Saartje->getIdeaFromBrain(i) != "FOOD!")
+			fail = true ;
+	if (fail == true)
+		std::cout << "Deep copy failed." << std::endl;
+	else
+		std::cout << "Deep copy successful" << std::endl;
 	delete Fred;
 	delete Saartje;
 }
