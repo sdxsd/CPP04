c09f9713e3270d610618743f63ee85fe81d656a9
sdxsd <willmaguire@icloud.com>
2023-03-03 20:16:41 +0100

    Tests are clearer.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,12 +7,17 @@ int main(void) {
 	const Animal* j = new Dog();
 	const Animal* i = new Cat();
 	const WrongAnimal* jeop = new WrongCat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
+	std::cout << "i->getType(): " << i->getType() << std::endl;
+	std::cout << "j->getType(): " << j->getType() << std::endl;
+	std::cout << "Cat says: ";
 	i->makeSound(); //will output the cat sound!
+	std::cout << "Dog says: ";
 	j->makeSound();
+	std::cout << "Meta says: ";
 	meta->makeSound();
+	std::cout << "The WrongCat says: ";
 	jeop->makeSound();
+	std::cout << "When it should be saying: * woem *" << std::endl;
 	delete meta;
 	delete j;
 	delete i;
